97d82039c9b893df54e523844029bff7ed8e3dca
k-dominik <k-dominik@users.noreply.github.com>
2020-07-09 09:49:01 +0200

    add some more fluff and actually run

--- galaxy/src/run_ilastik.py
+++ galaxy/src/run_ilastik.py
@@ -4,10 +4,14 @@ Small helper tool to run ilastik with a configuration file
 from pathlib import Path
 import json
 import logging
+import platform
+import subprocess
+import sys
 
 import click
 
 
+logging.basicConfig(stream=sys.stdout)
 logger = logging.getLogger()
 
 
@@ -32,28 +36,75 @@ class DefaultFromContextObj(click.Option):
         return super().get_default(ctx)
 
 
+class PathlibPath(click.Path):
+    def convert(self, value, param, ctx):
+        return Path(super().convert(value, param, ctx))
+
+
 @click.group()
-@click.option("--debug/--no-debug", default=False)
-@click.option("--config_file")
+@click.option(
+    "--verbose/--no-verbose",
+    help="Show output related to ilastik startup.",
+    default=False,
+)
+@click.option("--debug/--no-debug", help="Run ilastik in debug mode.", default=False)
+@click.option(
+    "--config_file",
+    type=PathlibPath(exists=True),
+    help="json/yaml config file with key-value pairs that match the commands options.",
+)
 @click.pass_context
-def main(ctx, debug, config_file=None):
-    if debug:
+def main(ctx, verbose, debug, config_file=None):
+    """
+    Run ilastik in headless mode with pre-trained projects
+
+    see the subcommand --help for a list of options.
+    Note, these options can be either defined via the respective command line option
+    (highest priority), an environment variable (COMMAND_LINE_OPTION_IN_UPPER-CASE),
+    or the config file.
+    """
+    if verbose:
         logger.setLevel(logging.DEBUG)
+    ctx.obj = {"debug": False}
     if config_file:
         with open(config_file, "r") as f:
             opts = json.load(f)
             # ctx.obj is used to determine values for arguments supplied
             # in the config file
-            ctx.obj = opts
+            ctx.obj.update(opts)
+    if debug:
+        ctx["debug"] = True
+
+
+_EXE_LOCATIONS = {
+    "Linux": "/path/to/ilastik-1.x.y-Linux/run_ilastik.sh",
+    "Darwin": "/path/to/ilastik-1.x.y-OSX.app",
+    "Windows": '"C:\\Program Files\\ilastik-1.x.y-win64\\ilastik.exe"',
+}
+
+_EXE_MESSAGE = f"Path to ilastik executable, e.g. {_EXE_LOCATIONS.get(platform.system(), 'warning: no default location for system could be determined.')}"
 
 
 @main.command("pixel_classification")
-@click.option("--ilastik_exe", required=True)
-@click.option("--project", required=True, cls=DefaultFromContextObj)
-@click.option("--raw_data", required=True, cls=DefaultFromContextObj)
+@click.option(
+    "--ilastik_exe",
+    help=_EXE_MESSAGE,
+    type=PathlibPath(exists=True),
+    required=True,
+    cls=DefaultFromContextObj,
+)
+@click.option(
+    "--project", required=True, type=PathlibPath(exists=True), cls=DefaultFromContextObj
+)
+@click.option(
+    "--raw_data",
+    required=True,
+    type=PathlibPath(exists=True),
+    cls=DefaultFromContextObj,
+)
 @click.option("--output_filename_format", required=True, cls=DefaultFromContextObj)
 @click.option("--input_axes", required=False, cls=DefaultFromContextObj)
-@click.option("--output_format", required=False, cls=DefaultFromContextObj)
+@click.option("--output_filename_format", required=False, cls=DefaultFromContextObj)
 @click.option("--cutout_subregion", required=False, cls=DefaultFromContextObj)
 @click.pass_context
 def run_headless_pixel_classification(
@@ -64,26 +115,31 @@ def run_headless_pixel_classification(
     output_filename_format: str,
     input_axes: str = "",
     output_format: str = "hdf5",
-    cutout_subregion: str = ""
+    cutout_subregion: str = "",
 ):
-    if not project.exists():
-        raise CMDPreconditionError(f"Could not find project file {project}.")
-    if not raw_data.exists():
-        raise CMDPreconditionError(f"Could not find raw data file {raw_data}")
+    """
+    Run ilastik pixel classification
+    """
+    if platform.system == "Darwin":
+        ilastik_exe = ilastik_exe / "Contents" / "ilastik-release" / "run_ilastik.sh"
 
     subprocess_args = [
-        ilastik_exe,
+        str(ilastik_exe),
         "--headless",
         "--project=" + str(project),
         "--raw_data=" + str(raw_data),
-        "--output_filename_format=" + str(output_filename_format),
+        "--output_filename_format=" + output_filename_format,
         "--output_format=" + output_format,
     ]
 
     if input_axes:
         subprocess_args.append("--input-axes=" + input_axes)
 
-    return subprocess_args
+    if ctx.obj["debug"]:
+        subprocess_args.append("--debug")
+    logger.debug("Subprocess arguments: %s", " ".join(subprocess_args))
+
+    subprocess.run(subprocess_args, check=True, encoding="utf-8")
 
 
 if __name__ == "__main__":
